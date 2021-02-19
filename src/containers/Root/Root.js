1bd999cc13c7e3d86e8996eb79efe9d454b4cd0e
sergey <sergei.rachmanin@gmail.com>
2021-02-19 04:08:24 +0300

    fix warnings

--- src/containers/Root/Root.js
+++ src/containers/Root/Root.js
@@ -1,22 +1,15 @@
-import React, { Suspense, lazy } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import RequestAccess from '../RequestAccess'
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from '../Login'
 
-
-const Login = lazy(() => import('../Login'))
-const Dashboard = lazy(() => import('../Dashboard'))
-
-const renderStartPage = () => {
-  return <Redirect to="dashboard/portfolio-builder/easy" />
-}
+// const Login = lazy(() => import('../Login'))
+// const Dashboard = lazy(() => import('../Dashboard'))
 
 const Root = () => {
   return (
     <Switch>
       <Suspense fallback={null}>
-        <Route exact path="/" render={renderStartPage} />
-        <Route exact path="/login" component={Login} />
-        <Route path="/requestAccess" component={RequestAccess} />
+        <Route path="/login" component={Login} />
       </Suspense>
     </Switch>
   )
