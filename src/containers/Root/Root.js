23fb3a1b2c1f7437483b1c96c0eb79d44f999f7b
sergey <sergei.rachmanin@gmail.com>
2021-02-20 21:19:06 +0300

    add html header

--- src/containers/Root/Root.js
+++ src/containers/Root/Root.js
@@ -1,14 +1,21 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Login from '../Login'
+import Login from '../Login';
+import HeaderBar from '../HeaderBar';
 
 // const Login = lazy(() => import('../Login'))
 // const Dashboard = lazy(() => import('../Dashboard'))
+
+const renderStartPage = () => {
+  return <Redirect to="dashboard" />
+}
 
 const Root = () => {
   return (
     <Switch>
       <Suspense fallback={null}>
+        <Route exact path="/" render={renderStartPage} />
+        <Route path="/dashboard" render={routeProps => <HeaderBar {...routeProps} />} />
         <Route path="/login" component={Login} />
       </Suspense>
     </Switch>
